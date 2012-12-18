c3057514cc2d77fcfcf43f47e94e670e70e05cac
David Rosson <david@rosson.com.au>
2012-12-18 20:04:26 +1100

    commit at hack night

---
+++ js/controllers.js
@@ -0,0 +1,25 @@
+
+function TokenCtrl($scope) {
+
+var strText = $scope.inputText || "Mary has a little lamb.";
+$scope.tokens = strText
+	.replace(/[^\w\s]|_/g, function ($1) { 
+											return ' ' + $1 + ' ';
+										}).replace(/[ ]+/g, ' ').trim.split(/ /);
+
+
+$scope.runText = function() {
+strText = $scope.inputText || "Mary has a little lamb.";
+$scope.tokens = strText
+	.replace(/[^\w\s]|_/g, function ($1) { 
+											return ' ' + $1 + ' ';
+										}).replace(/[ ]+/g, ' ').trim().split(/ /);
+};
+
+
+
+
+
+
+
+}
