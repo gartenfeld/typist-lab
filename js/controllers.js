9d710081ab813ae4c755cfdb60478c33dd4f4b89
David Rosson <david@rosson.com.au>
2012-12-19 00:07:32 +1100

    fixed bug

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,7 @@ var strText = $scope.inputText || "Mary has a little lamb.";
 $scope.tokens = strText
 	.replace(/[^\w\s]|_/g, function ($1) { 
 											return ' ' + $1 + ' ';
-										}).replace(/[ ]+/g, ' ').trim.split(/ /);
+										}).replace(/[ ]+/g, ' ').trim().split(/ /);
 
 
 $scope.runText = function() {
